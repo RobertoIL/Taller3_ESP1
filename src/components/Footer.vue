b0c6c78ddaa9c88967aa4d5cf717a18e4681c594
Roberto Illesca <robertillesca1@gmail.com>
2024-06-03 14:05:56 -0400

    footer simple

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,13 +1,7 @@
 <template>
         <footer class="bg-gray-800 text-white py-4">
         <div class="container mx-auto text-center">
-            <nav class="mb-4">
-                <a href="#" class="text-gray-400 hover:text-white mx-2">Home</a>
-                <a href="#" class="text-gray-400 hover:text-white mx-2">About</a>
-                <a href="#" class="text-gray-400 hover:text-white mx-2">Services</a>
-                <a href="#" class="text-gray-400 hover:text-white mx-2">Contact</a>
-            </nav>
-            <p class="text-gray-400">&copy; 2024 Your Company. All rights reserved.</p>
+            <p class="text-gray-400">&copy; 2024 MyWebsite. All rights reserved.</p>
         </div>
     </footer>
 </template>
